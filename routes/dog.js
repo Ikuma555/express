5f9a1bf054cb8e85c3a0a94480b43d6fc371e199
Ikuma555 <Amuki_GG@outlook.jp>
2025-12-04 02:19:57 +0900

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async (req, res) => {
-  request('https://dog.ceo/dog-api/', function (error, response,body) {
+  request('https://qiita.com/miriwo/items/c1727868d9225f4e4c4a', function (error, response,body) {
     if (!error && response.statusCode == 200) {
       const data = JSON.parse(body);
       res.json(data);
